10388bdba8834e5833143ba9aa4c3938b39d67d7
Alejandro Salinas V <87546710+AlejandroSalinasV@users.noreply.github.com>
2021-12-10 22:16:48 -0600

    Actualización de main
    Primera parte del proyecto de objeto seguro concluida.

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
 from proyecto import *
 
+"""
+PYTHON & LINUX. CURSOS DE TALENTO ESPECIALIZADO EN CIBERSEGURIDAD
+---	PROYECTO PARTE 1. OBJETO SEGURO	---
+ALEJANDRO SALINAS VICTORINO
+"""
+
 #  GENERAR OBJETOS
 Alejandro = ObjetoSeguro("Alejandro")
-BOB = ObjetoSeguro("BOB")
-#  comunicacion ejemplo
-print("SALUDAR")
-mensaje_c = Alejandro.saludar("BOB", "Hola me llamo ALEJANDRO SALINAS")
-BOB.esperar_respuesta(mensaje_c)
-respuesta = BOB.responder("Alejandro")
+Bob = ObjetoSeguro("Bob")
+
+#  INTERCAMBIO DE LLAVES
+print("[0] Intercambio de llaves entre objetos")
+Alejandro.monedero_llaves_pub(Bob.nombre, Bob.llave_publica())
+Bob.monedero_llaves_pub(Alejandro.nombre, Alejandro.llave_publica())
+
+#  COMUNICACION DE EJEMPLO
+print("[1] Alejandro saluda a Bob")
+mensaje_c = Alejandro.saludar("Bob", "Hola me llamo Alejandro Salinas")
+
+# Bob recibe el saludo de alejandro, y contesta automaticamente 
+print("[2 & 3]  Bob recibe el saludo de Alejandro, y contesta automaticamente")
+respuesta = Bob.esperar_respuesta(mensaje_c)
+
+# Alejandro recibe la respuesta
+print("[4] Alejandro recibe la respuesta")
 Alejandro.esperar_respuesta(respuesta)
+
+
+# siguimiento de la charla
+print("\n\t[...] Continuacion de la comunicacion [...]\n")
+print("[...] Alejandro le contesta a Bob")
+respuesta2 = Alejandro.responder("Espero que te encuentres bien")
+print("[...] Bob espera la respuesta")
+Bob.esperar_respuesta(respuesta2)
+print("[...] Bob le contesta a Alejandro")
+respuesta3 = Bob.responder("Claro que me encuentro bien. Salu2")
+print("[...] Alejandro espera la respuesta")
+Alejandro.esperar_respuesta(respuesta3)
 
 # Otros elementos para comprobar el registro de la comunicacion
-print("COMPROBAR LA CONSULTA DE ARCHIVOS")
-respuesta = BOB.responder("Alejandro")
-Alejandro.esperar_respuesta(respuesta)
-respuesta = BOB.responder("Alejandro")
-Alejandro.esperar_respuesta(respuesta)
-
-Alejandro.consultar_msj(2)
-Alejandro.consultar_msj(1)
+print("\n\t---COMPROBAR LA CONSULTA DE ARCHIVOS---")
+print(f"Mensajes recibidos de {Alejandro.nombre}")
 Alejandro.consultar_msj(0)
+Alejandro.consultar_msj(1)
+Alejandro.consultar_msj(2)  # en caso de que no exista dicha linea, no retorna nada
+Alejandro.consultar_msj(3)
+
+print("--------------------\t----------------------")
+print(f"Mensajes recibidos de {Bob.nombre}")
+Bob.consultar_msj(0)
+Bob.consultar_msj(1)
